220be664932fc475493f14b7c0b7917a6445adc3
james russell <jamesrussell@pursuit.org>
2022-02-18 15:51:07 -0500

    re-seeded due to img err

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO products (name, description, price, rating, featured, image) VALUES
-('Yeezy 350', 'Oat & Fire', 220, 5, true, 'https://www.kicksonfire.com/wp-content/uploads/2021/06/adidas-yeezy-boost-350-v2-mx-oat-9.jpeg'),
+('Yeezy 350', 'Oat & Fire', 220, 5, true, 'https://sneakerbardetroit.com/wp-content/uploads/2021/06/adidas-Yeezy-Boost-350-V2-MX-Oat-GW3773-On-Foot.jpg'),
 ('Yeezy 350', 'Yecheil', 220, 5, true, 'https://fastsole.co.uk/wp-content/uploads/2019/09/Here-Is-The-First-On-Foot-Look-At-The-adidas-Yeezy-Boost-350-V2-Yecheil.jpg'),
 ('Knit Runner Boot', 'Sulpher', 400, 5, true, 'https://seangoca.files.wordpress.com/2021/11/dsc07262b.jpg?w=994&h=&zoom=2'),
 ('Yeezy Foam Runner', 'Sand', 75, 5, true, 'https://cdn.sanity.io/images/c1chvb1i/production/a1e9927add952a1de190db03688b7d8efd789f10-1100x735.jpg/191592420_1457073971293656_3883658647910884946_n.jpg'),
